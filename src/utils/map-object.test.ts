a94a7dee23a08706b841a0753422af27ad7e189b
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2022-06-18 18:04:25 +0200

    [dsch] lint fix

--- src/utils/map-object.test.ts
+++ src/utils/map-object.test.ts
@@ -18,7 +18,8 @@ describe('mapOject', () => {
     type LazyObj<T extends object> = { [p in keyof T]: Lazy<T[p]> }
 
     const result = mapObject<typeof obj, keyof typeof obj, LazyObj<typeof obj>>(
-      obj, (name, self) => () => self[name] as any,
+      obj,
+      (name, self) => () => self[name] as any,
     );
 
     expectStrictType<() => number>(result.x);
@@ -37,9 +38,7 @@ describe('mapOject', () => {
     type T = typeof obj;
     type D = LazyObj<Pick<T, N>>;
 
-    const result = mapObject<T, N, D>(
-      obj, (name, self) => () => self[name] as any, ['x', 'y'],
-    );
+    const result = mapObject<T, N, D>(obj, (name, self) => () => self[name] as any, ['x', 'y']);
 
     expectStrictType<{ x:() => number; y: () => number; }>(result);
     expect(result.x).toBeInstanceOf(Function);
